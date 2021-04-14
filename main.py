f388ae77a3734310b059cfa1c68520e5497caebe
hsiec <huberts94@gmail.com>
2021-04-14 23:31:13 +0200

    Added medium level AI

--- main.py
+++ main.py
@@ -1,162 +1,174 @@
 from random import choice
 
 
-class TTTGame:
+class TicTacToe:
     GAME_PARAMS = ['medium', 'easy', 'user', 'hard']
 
     def __init__(self):
-        self.table = ['_', '_', '_',
-                      '_', '_', '_',
-                      '_', '_', '_']
-        self.sign = 'X'
-        self.bot_1 = None
-        self.bot_2 = None
-        self.game_mode = None
+        self.table = None
+        self.player_1 = None
+        self.player_2 = None
+        self.turn = 1
         self.menu()
 
     def menu(self):
-        self.reset_game()
         command = input('Input command: ')
         if command == 'exit':
             pass
         else:
             try:
-                cmd, p1, p2 = command.split()
-                if cmd == 'start' and all(param in self.GAME_PARAMS for param in {p1, p2}):
-                    if p1 == p2 == 'user':
-                        self.game_mode = 'pvp'  # player vs player
-                    elif p1 != p2 and 'user' in (p1, p2):
-                        self.game_mode = 'pvb'  # player vs bot
-                        if p1 == 'user':
-                            self.set_bots(bot1=p2)
-                        else:
-                            self.set_bots(bot1=p1)
-                    else:
-                        self.game_mode = 'bvb'
-                        self.set_bots(p1, p2)
-                    self.game()
+                cmd, player1, player2 = command.split()
+                if cmd == 'start' and all(param in self.GAME_PARAMS for param in [player1, player2]):
+                    self.setup_game(player1=player1, player2=player2)
                 else:
                     raise ValueError
-
             except ValueError:
                 print('Bad parameters!')
                 self.menu()
 
-    def set_bots(self, bot1=None, bot2=None):
-        bot_list = {'easy': self.EasyBot(),
-                    'medium': self.MediumBot()}
-        if all([bot1, bot2]):
-            self.bot_1 = bot_list[bot1]
-            self.bot_2 = bot_list[bot2]
-        else:
-            self.bot_1 = bot_list[bot1]
+    def setup_game(self, player1, player2):
+        self.table = self.Table()
+        self.player_1 = self.setup_player(player1, sign='X', opp='O')
+        self.player_2 = self.setup_player(player2, sign='O', opp='X')
+        self.turn = 1
+        self.game()
 
-    def reset_game(self):
-        self.table = ['_', '_', '_',
-                      '_', '_', '_',
-                      '_', '_', '_']
-        self.sign = 'X'
+    def setup_player(self, player_type, sign, opp):
+        if player_type == 'user':
+            return self.HumanPlayer(sign=sign)
+        else:
+            return self.Bot(level=player_type, table=self.table, sign=sign, opp=opp)
 
     def game(self):
-        self.print_table(self.table)
-        if self.game_mode == 'pvp':
-            self.enter_cords()
-
-        elif self.game_mode == 'bvb':
-            if self.sign == 'X':
-                self.update_table(self.bot_1.make_move(self.table))
-            elif self.sign == 'O':
-                self.update_table(self.bot_2.make_move(self.table))
-
-        elif self.game_mode == 'pvb':
-            if self.sign == 'X':
-                self.enter_cords()
-            else:
-                self.update_table(self.bot_1.make_move(self.table))
-
+        self.table.print_table()
+        self.table.update_table(*self.move())
         self.check_game_state()
+        self.game()
 
-    def enter_cords(self):
-        try:
-            x, y = input('Enter the coordinates: ').split()
-            if x.isdigit() is False or y.isdigit() is False:
-                print('You should enter numbers!')
-                self.enter_cords()
-            elif x not in '123' or y not in '123':
-                print('Coordinates should be from 1 to 3!')
-                self.enter_cords()
-            else:
-                index = self.transfer_cord(x, y)
-                if self.table[index] == '_':
-                    self.update_table(index)
-                else:
-                    print('This cell is occupied! Choose another one!')
-                    self.enter_cords()
-        except ValueError:
-            print('You should enter numbers!')
-            self.enter_cords()
-
-    def update_table(self, index):
-        self.table[index] = self.sign
-        self.sign = self.sign_change(current_sign=self.sign)
+    def move(self):
+        if self.turn == 1:
+            self.turn = 2
+            return self.player_1.make_move(self.table.get_empty())
+        else:
+            self.turn = 1
+            return self.player_2.make_move(self.table.get_empty())
 
     def check_game_state(self):
-        vertical_win = [''.join(self.table[i: i + 3]) for i in range(0, 8, 3)]
-        horizontal_win = [''.join(self.table[i: 9:3]) for i in range(3)]
-        cross_win = [''.join(self.table[0:9:4]), ''.join(self.table[2:7:2])]
-
-        win = vertical_win + horizontal_win + cross_win
-        if any(['XXX' in win, 'OOO' in win, '_' not in self.table]):
-            if 'XXX' in win:
-                self.print_table(self.table)
+        win = self.table.get_win_cells()
+        x_win = ['X'] * 3 in win
+        o_win = ['O'] * 3 in win
+        if any([x_win, o_win, self.table.get_empty() is None]):
+            self.table.print_table()
+            if x_win:
                 print('X wins')
-            elif 'OOO' in win:
-                self.print_table(self.table)
+            elif o_win:
                 print('O wins')
-            elif '_' not in self.table:
-                self.print_table(self.table)
+            else:
                 print('Draw')
             self.menu()
-        else:
-            self.game()
 
-    @staticmethod
-    def sign_change(current_sign):
-        return 'X' if current_sign == 'O' else 'O'
+    class Table:
+        def __init__(self):
+            self.table = ['_', '_', '_',
+                          '_', '_', '_',
+                          '_', '_', '_']
 
-    @staticmethod
-    def transfer_cord(x, y):
-        return 3 * (int(x) - 1) + (int(y) - 1)
+        def update_table(self, *params):
+            index, sign = params
+            self.table[index] = sign
 
-    @staticmethod
-    def print_table(table):
-        print('---------')
-        for i in range(0, 8, 3):
-            print('| ' + ' '.join(table[i: i + 3]) + ' |')
-        print('---------')
+        def get_positions(self):
+            num_table = [index if sign == '_' else sign for index, sign in enumerate(self.table)]
+            return self.get_win_cells(num_table)
 
-    class EasyBot:
+        def get_empty(self):
+            empty = [index for index, sign in enumerate(self.table) if sign == '_']
+            return empty if empty else None
+
+        def get_win_cells(self, table=None):
+            if table is None:
+                table = self.table
+            vertical_win = [table[i: i + 3] for i in range(0, 8, 3)]
+            horizontal_win = [table[i: 9:3] for i in range(3)]
+            cross_win = [table[0:9:4], table[2:7:2]]
+            return vertical_win + horizontal_win + cross_win
+
+        def print_table(self):
+            print('---------')
+            for i in range(0, 8, 3):
+                print('| ' + ' '.join(self.table[i: i + 3]) + ' |')
+            print('---------')
+
+    class _Player:
+        def make_move(self, empty_cells):
+            pass
+
+    class HumanPlayer(_Player):
+
+        def __init__(self, sign):
+            self.own_sign = sign
+
+        def make_move(self, empty_cells):
+            try:
+                x, y = input('Enter the coordinates: ').split()
+                if x.isdigit() is False or y.isdigit() is False:
+                    print('You should enter numbers!')
+
+                elif x not in '123' or y not in '123':
+                    print('Coordinates should be from 1 to 3!')
+
+                else:
+                    index = self.transfer_cord(x, y)
+                    if index in empty_cells:
+                        return index, self.own_sign
+                    else:
+                        print('This cell is occupied! Choose another one!')
+
+                return self.make_move(empty_cells)
+
+            except ValueError:
+                print('You should enter numbers!')
+                return self.make_move(empty_cells)
 
         @staticmethod
-        def get_emptycells(table):
-            return [index for index, sign in enumerate(table) if sign == '_']
+        def transfer_cord(x, y):
+            return 3 * (int(x) - 1) + (int(y) - 1)
 
-        def make_move(self, table):
-            empty_cells = self.get_emptycells(table)
-            print('Making move level "easy"')
-            return choice(empty_cells)
+    class Bot(_Player):
 
-    class MediumBot:
+        def __init__(self, level, table, sign, opp):
+            self.level = level
+            self.table = table
+            self.own_sign = sign
+            self.opp_sign = opp
+
+        def make_move(self, empty_cells):
+            print(f'Making move level "{self.level}"')
+            move = choice(empty_cells)
+            if self.level == 'easy':
+                pass
+
+            elif self.level == 'medium':
+                best_move = self.analyze_table()
+                if best_move:
+                    move = choice(best_move)
+
+            return move, self.own_sign
+
+        def analyze_table(self):
+
+            positions = self.table.get_positions()
+            to_win_pos = self.filter_pos(sign=self.own_sign, pos=positions)
+            to_block_pos = self.filter_pos(sign=self.opp_sign, pos=positions)
+            return to_win_pos if to_win_pos else to_block_pos
 
         @staticmethod
-        def get_emptycells(table):
-            return [index for index, sign in enumerate(table) if sign == '_']
+        def filter_pos(sign, pos):
+            pos_list = list(filter(
+                lambda x: x.count(sign) == 2, pos))
+            positions = [item for item_list in pos_list for item in item_list if type(item) == int]
 
-        def make_move(self, table):
-            empty_cells = self.get_emptycells(table)
-            print('Making move level "medium"')
-
-            return choice(empty_cells)
+            return positions
 
 
-TTTGame()
+TicTacToe()
